6864f338c154cf98f1cb43c310a0ae6e442fd985
Tara Culpin <taraculpin@gmail.com>
2022-02-16 13:51:08 +0000

    Added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"];
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    category: categories.sample
+  )
+  restaurant.save
+end
